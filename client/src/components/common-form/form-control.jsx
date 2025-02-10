8eaf458221a3500822296e75a78c369985fc6f31
Srijita Das <srijita.sriz@gmail.com>
2025-02-10 18:56:46 +0530

    Initial commit

---
+++ client/src/components/common-form/form-control.jsx
@@ -0,0 +1,97 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { Textarea } from "../ui/textarea";
+
+function FormControls({ formControls = [], formData = {}, setFormData }) {
+  // Ensure formData is an object before rendering
+  const handleChange = (event, controlName) => {
+    const value = event.target.value;
+    setFormData(prevState => ({
+      ...prevState,
+      [controlName]: value
+    }));
+  };
+
+  function renderComponentByType(getControlItem) {
+    let element = null;
+    const currentControlItemValue = formData[getControlItem.name] || "";
+
+    switch (getControlItem.componentType) {
+      case "input":
+        element = (
+          <Input
+            id={getControlItem.name}
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            type={getControlItem.type}
+            value={currentControlItemValue}
+            onChange={(event) => handleChange(event, getControlItem.name)}
+          />
+        );
+        break;
+      case "select":
+        element = (
+          <Select
+            onValueChange={(value) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: value,
+              })
+            }
+            value={currentControlItemValue}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={getControlItem.label} />
+            </SelectTrigger>
+            <SelectContent>
+              {getControlItem.options?.map((optionItem) => (
+                <SelectItem key={optionItem.id} value={optionItem.id}>
+                  {optionItem.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        );
+        break;
+      case "textarea":
+        element = (
+          <Textarea
+            id={getControlItem.name}
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            value={currentControlItemValue}
+            onChange={(event) => handleChange(event, getControlItem.name)}
+          />
+        );
+        break;
+      default:
+        element = (
+          <Input
+            id={getControlItem.name}
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            type={getControlItem.type}
+            value={currentControlItemValue}
+            onChange={(event) => handleChange(event, getControlItem.name)}
+          />
+        );
+        break;
+    }
+
+    return element;
+  }
+
+  return (
+    <div className="flex flex-col gap-3">
+      {formControls.map((controleItem) => (
+        <div key={controleItem.name}>
+          <Label htmlFor={controleItem.name}>{controleItem.label}</Label>
+          {renderComponentByType(controleItem)}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FormControls;
